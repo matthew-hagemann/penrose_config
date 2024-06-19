709621f07a17cedab1f869067fdc043dbf1c8687
Matt Hagemann <matt3hagemann@gmail.com>
2024-06-19 08:08:08 +0200

    stealing a status bar!

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
-//! penrose :: minimal configuration
-//!
-//! This file will give you a functional if incredibly minimal window manager that
-//! has multiple workspaces and simple client / workspace movement.
 use penrose::{
     builtin::{
-        actions::{exit, modify_with, send_layout_message, spawn},
-        layout::messages::{ExpandMain, IncMain, ShrinkMain},
+        actions::{exit, log_current_state, modify_with, send_layout_message, spawn},
+        layout::{
+            messages::{ExpandMain, IncMain, ShrinkMain},
+            transformers::{Gaps, ReserveTop},
+            MainAndStack, Monocle,
+        },
     },
     core::{
         bindings::{parse_keybindings_with_xmodmap, KeyEventHandler},
+        layout::LayoutStack,
         Config, WindowManager,
     },
-    map,
+    extensions::hooks::add_ewmh_hooks,
+    map, stack,
     x11rb::RustConn,
     Result,
 };
+use penrose_ui::{bar::Position, core::TextStyle, status_bar};
 use std::collections::HashMap;
 use tracing_subscriber::{self, prelude::*};
 
+const FONT: &str = "ProFontIIx Nerd Font";
+const BLACK: u32 = 0x282828ff;
+const WHITE: u32 = 0xebdbb2ff;
+const GREY: u32 = 0x3c3836ff;
+const BLUE: u32 = 0x458588ff;
+
+const MAX_MAIN: u32 = 1;
+const RATIO: f32 = 0.6;
+const RATIO_STEP: f32 = 0.1;
+const OUTER_PX: u32 = 5;
+const INNER_PX: u32 = 5;
+const BAR_HEIGHT_PX: u32 = 18;
+
 fn raw_key_bindings() -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
     let mut raw_bindings = map! {
-        map_keys: |k: &str| k.to_string();
+        map_keys: |k: &str| k.to_owned();
 
         "M-j" => modify_with(|cs| cs.focus_down()),
         "M-k" => modify_with(|cs| cs.focus_up()),
@@ -32,12 +48,13 @@ fn raw_key_bindings() -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
         "M-bracketleft" => modify_with(|cs| cs.previous_screen()),
         "M-grave" => modify_with(|cs| cs.next_layout()),
         "M-S-grave" => modify_with(|cs| cs.previous_layout()),
-        "M-S-Up" => send_layout_message(|| IncMain(1)),
-        "M-S-Down" => send_layout_message(|| IncMain(-1)),
-        "M-S-Right" => send_layout_message(|| ExpandMain),
-        "M-S-Left" => send_layout_message(|| ShrinkMain),
+        "M-Up" => send_layout_message(|| IncMain(1)),
+        "M-Down" => send_layout_message(|| IncMain(-1)),
+        "M-Right" => send_layout_message(|| ExpandMain),
+        "M-Left" => send_layout_message(|| ShrinkMain),
         "M-semicolon" => spawn("dmenu_run"),
-        "M-Return" => spawn("zsh"),
+        "M-S-s" => log_current_state(),
+        "M-Return" => spawn("st"),
         "M-A-Escape" => exit(),
     };
 
@@ -57,29 +74,43 @@ fn raw_key_bindings() -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
     raw_bindings
 }
 
+fn layouts() -> LayoutStack {
+    stack!(
+        MainAndStack::side(MAX_MAIN, RATIO, RATIO_STEP),
+        MainAndStack::side_mirrored(MAX_MAIN, RATIO, RATIO_STEP),
+        MainAndStack::bottom(MAX_MAIN, RATIO, RATIO_STEP),
+        Monocle::boxed()
+    )
+    .map(|layout| ReserveTop::wrap(Gaps::wrap(layout, OUTER_PX, INNER_PX), BAR_HEIGHT_PX))
+}
+
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
-        .with_env_filter("info")
+        .with_env_filter("debug")
         .finish()
         .init();
 
+    let config = add_ewmh_hooks(Config {
+        default_layouts: layouts(),
+        ..Config::default()
+    });
+
     let conn = RustConn::new()?;
     let key_bindings = parse_keybindings_with_xmodmap(raw_key_bindings())?;
-    let wm = WindowManager::new(Config::default(), key_bindings, HashMap::new(), conn)?;
+    let style = TextStyle {
+        fg: WHITE.into(),
+        bg: Some(BLACK.into()),
+        padding: (2, 2),
+    };
+
+    let bar = status_bar(BAR_HEIGHT_PX, FONT, 8, style, BLUE, GREY, Position::Top).unwrap();
+
+    let wm = bar.add_to(WindowManager::new(
+        config,
+        key_bindings,
+        HashMap::new(),
+        conn,
+    )?);
 
     wm.run()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn bindings_parse_correctly_with_xmodmap() {
-        let res = parse_keybindings_with_xmodmap(raw_key_bindings());
-
-        if let Err(e) = res {
-            panic!("{e}");
-        }
-    }
 }
